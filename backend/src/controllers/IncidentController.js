8a6f51310b62ebae0c0ce7f62b3d9da0614d5919
wallacesw11 <wallace.dsn.erp@gmail.com>
2020-03-28 19:34:41 -0300

    Atualizações

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -5,15 +5,15 @@ module.exports = {
     const { page = 1 } = request.query;
 
     const [count] = await connection('incidents').count();
-    
+
 
     const incidents = await connection('incidents')
       .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
       .limit(5)
-      .offset((page - 1) * 5)      
+      .offset((page - 1) * 5)
       .select([
-        'incidents.*', 
-        'ongs.name', 
+        'incidents.*',
+        'ongs.name',
         'ongs.email',
         'ongs.whatsapp',
         'ongs.city',
@@ -49,7 +49,7 @@ module.exports = {
       .first();
 
     if (incident.ong_id !== ong_id) {
-      return response.status(401).json({ erro: 'Operation not permitted.'});
+      return response.status(401).json({ erro: 'Operation not permitted.' });
     }
 
     await connection('incidents').where('id', id).delete();
--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -4,16 +4,13 @@ module.exports = {
   async create(request, response) {
     const { id } = request.body;
 
-    console.log('id: ' + id);
-    
-
     const ong = await connection('ongs')
       .where('id', id)
       .select('name')
       .first();
 
     if (!ong) {
-      return response.status(400).json({ error: 'No ONG found with this ID'});
+      return response.status(400).json({ error: 'No ONG found with this ID' });
     }
 
     return response.json(ong);
